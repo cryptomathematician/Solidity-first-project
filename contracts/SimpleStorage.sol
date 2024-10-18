cbd7d1aae5e1f461e0e77645b03074a74a98d4ce
cryptomathematician <bosman006@st.ug.edu.gh>
2024-10-18 17:27:25 +0000

    Storing information on memory

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,22 +4,28 @@ pragma solidity  ^0.8.8 ; // solidity version
 contract SimpleStorage {
     // boolean, unint, int, adress, bytes
     uint256 public favoriteNumber; // gets initialized to zero
-    People public person = People({favoriteNumber:2, name: "Patrick"});
-
+    
     struct People {
         uint256 favoriteNumber;
         string name;
     }
 
+    People[] public people; 
+
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
-        favoriteNumber = favoriteNumber;
     }
 
    function retrieve()  public view returns (uint256)   {
     return favoriteNumber;
    }
+
+   function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        people.push(newPerson);
+   }
     
 }
+
 
 // 0x48B94361FD30900Bd91a6C96013c9a8d07c670cd
