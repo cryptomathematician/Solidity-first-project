7684ee187a86c7752092380b88ac3409aebb6dd5
cryptomathematician <bosman006@st.ug.edu.gh>
2024-10-20 11:30:55 +0000

    Done with the storage factory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -6,9 +6,20 @@ import "./SimpleStorage.sol";
 
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
     function createSimpleStorageContract() public{
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet (uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
     }
 }
-
+  
