313919abd80ada2aecd41f7d4669f8a45e467b38
cryptomathematician <bosman006@st.ug.edu.gh>
2024-10-22 17:08:08 +0000

    working on price feed

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0 ;
 // withdraw funds 
 // set a minimum funding value in USD
 
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     function fund() public payable {
         
@@ -15,9 +17,16 @@ contract FundMe {
     function getPrice() public {
         // ABI
         // addresses 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        
+    }
+
+    function getVersion() public view returns(uint256) {
+        AggregatorV3Interface priceFeed =  AggregatorV3Interface( 0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
     }
 
     function getConversionRate() public {}
+
     function withdraw() public{}
 
 }
