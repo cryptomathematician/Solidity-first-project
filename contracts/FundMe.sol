6c06d3df279c2e0c44bb5c6387c8224d4914ac65
cryptomathematician <bosman006@st.ug.edu.gh>
2024-10-22 22:42:21 +0000

    done with conversion of ethereum to Usd

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,16 +8,20 @@ pragma solidity ^0.8.0 ;
 import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
+    uint256 public minimumUsd = 50 * 1e18;
     function fund() public payable {
         
-        require(msg.value > 1e18, "Didn't send enough for the transaction");
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough for the transaction");
     }
 
 
-    function getPrice() public {
+    function getPrice() public view returns(uint256){
         // ABI
         // addresses 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        
+        AggregatorV3Interface priceFeed =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+
+        return uint256(price * 1e10);
     }
 
     function getVersion() public view returns(uint256) {
@@ -25,7 +29,12 @@ contract FundMe {
         return priceFeed.version();
     }
 
-    function getConversionRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = ethPrice * ethAmount/ 1e18;
+
+        return ethAmountInUsd;
+    }
 
     function withdraw() public{}
 
