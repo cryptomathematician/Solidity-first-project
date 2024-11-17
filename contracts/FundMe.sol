a1d2ea2bd289a1c9f9c0ce08ac781797382899c9
cryptomathematician <bosman006@st.ug.edu.gh>
2024-11-17 01:02:04 +0000

    making an owner withdraw funds in solidity

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -13,6 +13,12 @@ contract FundMe {
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+
+    address public owner ;
+
+    constructor(){
+        owner =msg.sender;
+    }
     function fund() public payable {
         
         require(msg.value.getConversionRate() >= minimumUsd, "Didn't send enough for the transaction");
@@ -23,7 +29,15 @@ contract FundMe {
 
     
     function withdraw() public{
+        require(msg.sender == owner);
+        for( uint256 funderIndex=0; funderIndex < funders.length ; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder]= 0;
+        }
         
+        funders = new address[] (0);
+        (bool callSuccess) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed")
     }
 
 }
